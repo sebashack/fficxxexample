8972c5d2005f9d8ad51544030cd9059a28e7c2c5
sebashack <sebastian0092@gmail.com>
2018-10-28 13:23:41 -0500

    {NL, SP} simplify generator code

--- src/Generator.hs
+++ src/Generator.hs
@@ -27,26 +27,23 @@ import FFICXX.Generate.Type.Config
   )
 import FFICXX.Generate.Type.PackageInterface (HeaderName(..))
 
-genCabal :: IO Cabal
-genCabal = do
-  srcContents <- readFile "/home/sebastian/cppBindings/cpplib/src/A.cpp"
-  let srcs = [AddCSrc "A.cpp" srcContents]
-  return
-    Cabal
-      { cabal_pkgname = CabalName "Bindings"
-      , cabal_version = "0.0"
-      , cabal_cheaderprefix = "Bindings"
-      , cabal_moduleprefix = "Bindings"
-      , cabal_additional_c_incs = []
-      , cabal_additional_c_srcs = []
-      , cabal_license = Just "BSD3"
-      , cabal_licensefile = Just "LICENSE"
-      , cabal_extraincludedirs = ["/home/sebastian/cppBindings/cpplib/include"]
-      , cabal_extralibdirs = ["/home/sebastian/cppBindings/cpplib/lib"]
-      , cabal_extrafiles = []
-      , cabal_additional_pkgdeps = []
-      , cabal_pkg_config_depends = []
-      }
+configCabal :: Cabal
+configCabal =
+  Cabal
+    { cabal_pkgname = CabalName "Bindings"
+    , cabal_version = "0.0"
+    , cabal_cheaderprefix = "Bindings"
+    , cabal_moduleprefix = "Bindings"
+    , cabal_additional_c_incs = []
+    , cabal_additional_c_srcs = []
+    , cabal_license = Just "BSD3"
+    , cabal_licensefile = Just "LICENSE"
+    , cabal_extraincludedirs = ["/home/sebastian/cppBindings/cpplib/include"]
+    , cabal_extralibdirs = ["/home/sebastian/cppBindings/cpplib/lib"]
+    , cabal_extrafiles = []
+    , cabal_additional_pkgdeps = []
+    , cabal_pkg_config_depends = []
+    }
 
 classAConstructor :: Function
 classAConstructor = Constructor {func_args = [], func_alias = Nothing}
@@ -56,10 +53,10 @@ method1Binding =
   NonVirtual
     {func_ret = Void, func_name = "foo", func_args = [], func_alias = Nothing}
 
-classA :: Cabal -> Class
-classA cabal = do
+classA :: Class
+classA =
   Class
-    { class_cabal = cabal
+    { class_cabal = configCabal
     , class_name = "A"
     , class_parents = []
     , class_protected = Protected []
@@ -72,35 +69,34 @@ classA cabal = do
 classBConstructor :: Function
 classBConstructor = Constructor {func_args = [], func_alias = Nothing}
 
-method2Binding :: Cabal -> Function
-method2Binding cabal =
+method2Binding :: Function
+method2Binding =
   Virtual
-    { func_ret = cppclass_ $ classA cabal
+    { func_ret = cppclass_ $ classA
     , func_name = "method2"
-    , func_args = [cppclass (classA cabal) "x"]
+    , func_args = [cppclass classA "x"]
     , func_alias = Nothing
     }
 
-classB :: Cabal -> Class
-classB cabal =
+classB :: Class
+classB =
   Class
-    { class_cabal = cabal
+    { class_cabal = configCabal
     , class_name = "B"
-    , class_parents = [classA cabal]
+    , class_parents = [classA]
     , class_protected = Protected []
     , class_alias = Nothing
-    , class_funcs = [classBConstructor, method2Binding cabal]
+    , class_funcs = [classBConstructor, method2Binding]
     , class_vars = []
     , class_tmpl_funcs = []
     }
 
 genBindings :: IO ()
 genBindings = do
-  cabal <- genCabal
   simpleBuilder
     "Bindings"
     (ModuleUnitMap $
      HM.singleton (MU_Class "A") (ModuleUnitImports [] [HdrName "A.h"]))
-    (cabal, [classA cabal], [], [])
+    (configCabal, [classA], [], [])
     ["MyLib"]
     []
