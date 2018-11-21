838ccddcfce8aeb00278eb6e0ccedf7d566a1af2
sebashack <sebastian0092@gmail.com>
2018-11-21 12:18:28 -0500

    Ship a distribution that does not depend on LD_LIBRARY_PATH

--- exampleBindings/genBindings.sh
+++ exampleBindings/genBindings.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 CURRENT_DIR="$( readlink -f "$( dirname "${BASH_SOURCE[0]}" )" )"
-export LD_LIBRARY_PATH="${CURRENT_DIR}/cpplib/lib"
+export LD_LIBRARY_PATH="${CURRENT_DIR}/dist/lib"
 cd ..
 stack install
 cd exampleBindings
 make -C cpplib
 rm -rf Bindings working
 generator
-stack build
-stack exec example
+mkdir dist
+stack install --local-bin-path "${CURRENT_DIR}/dist" --ghc-options=-optl-Wl,-rpath,'$ORIGIN/lib'
